cc928d51775c2eb0aa5fb7a4d32c24916ab2bb71
kodylow <kodylow7@gmail.com>
2022-05-26 13:28:45 -0700

    implemented multithreading logic

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,15 @@ use std::net::{TcpListener, TcpStream};
 use std::time::Duration;
 use std::{fs, thread};
 
+use hello::ThreadPool;
+
 fn main() {
     // bind works like new, returns a new instance of TcpListener
     // returns a Result<T, E> because binding might require administrator privileges
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    // create finite threadpool
+    let pool = ThreadPool::new(4);
 
     // a stream represents an open connection between client and server
     // a connection is the name for the full request response process
@@ -15,7 +20,11 @@ fn main() {
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
+
+        println!("Shutting down.");
     }
 }
 
@@ -25,6 +34,7 @@ fn handle_connection(mut stream: TcpStream) {
     stream.read(&mut buffer).unwrap();
 
     let get = b"GET / HTTP/1.1\r\n";
+    // simulate slow request
     let sleep = b"GET /sleep HTTP/1.1\r\n";
 
     let (status_line, filename) = if buffer.starts_with(get) {
